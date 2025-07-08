7ccb5a7ec01af2cf4e6c61a4376aa07fbb6ca34d
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-08 15:55:58 +0200

    [UPD] - (iniziali):check

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const everyFirstLetter = (array) => {
+  let capitalArray = [];
+  for (i = 0; i<array.length; i++) {
+    let firstLetter = array[i].slice(0,1)
+    capitalArray.push(firstLetter)
+  }
+  return capitalArray
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const newArray = everyFirstLetter(names)
+console.log(newArray)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
