d791fcbcddcb8fb4566e2c7f02a80e515696ef8e
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-08 16:18:09 +0200

    [UPD] - (filtra iniziali):check

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+let result = [];
+const namesStartingWith = (array, letter) => {
+  for (i=0; i<array.length; i++) {
+    let firstLetter = array[i].slice(0,1);
+    if (firstLetter === letter) {
+      result.push(array[i])
+    }
+  }
+  return result
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let namesStartingWithA = namesStartingWith(names, `A`)
+console.log(namesStartingWithA)
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
