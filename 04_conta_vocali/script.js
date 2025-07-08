34b98fdb516b1dbab093f6103031f66ffc1ea8e5
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-08 16:40:14 +0200

    [UPD] - (conta vocali):check

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,23 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+let array = [];
+const vocals = ["a","e","i","o","u"];
+let vocalsCount = [];
+
+vocalsIn = (text) => {
+  array = text.split("");
+  for (i=0; i<array.length; i++) {
+    if ( vocals.indexOf(array[i]) !== -1) {
+      vocalsCount.push(array[i])
+    }
+  }
+  return vocalsCount
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let vocalsCountWord = vocalsIn (word)
+console.log(vocalsCountWord)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
